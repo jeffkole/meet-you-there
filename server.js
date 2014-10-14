4c24d6f77c521ac515d798abbc62e781fed8d3dd
scottjason <scottleejason@gmail.com>
2014-10-13 22:51:54 -0700

    creaets new stream session, token, auth on server

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ var express = require('express')
  ,  path = require('path')
  ,  debug = require('debug')('server')
  ,  bodyParser = require('body-parser')
+ ,  dotenv = require('dotenv')
+ ,   OpenTok = require('opentok')
  ,  app = express();
+
+dotenv.load();
+opentok = new OpenTok( process.env.apiKey, process.env.apiSecret );
+
+
 
 app.set( 'port', process.env.PORT || 3000 );
 
@@ -12,9 +19,24 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({ extended: true } ));
 
+opentok.createSession(function( err, session ) {
+  if ( err ) throw err;
+  app.set('sessionId', session.sessionId);
+  // init is the callback to invoke the app
+  init();
+});
+
+ app.get('/', function( req, res ){
+    streamAuth =  { apiKey: process.env.apiKey, sessionId: process.env.apiSecret, token: process.env.token }
+    console.log( )
+    res.sendfile( streamAuth );
+    });
+
+
+function init() {
 var server = app.listen( app.get( 'port' ), function() {
   debug( 'Express server listening on port ' + server.address().port );
   console.log( "Express server listening on port", server.address().port  )
-});
-
+})
+}
 exports = module.exports = app;
