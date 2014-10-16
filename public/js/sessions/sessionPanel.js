c1ac7ee16363c655216b284c611b0e906bbeb594
scottjason <scottleejason@gmail.com>
2014-10-15 20:08:08 -0700

    removes duplicate stream containers, lints css

--- public/js/sessions/sessionPanel.js
+++ public/js/sessions/sessionPanel.js
@@ -2,11 +2,11 @@ function SessionPanel() {};
 
 SessionPanel.prototype = {
   renderPanel : function( session, publisher, callback ){
-    var parentDiv = document.getElementById('publisherContainer');
-    var div = document.createElement('div');
-    div.setAttribute('id', 'opentok_publisher');
-    parentDiv.appendChild( div );
-    publisher.addEventListener('settingsButtonClick', callback );
+    // var parentDiv = document.getElementById('publisherContainer');
+    // var div = document.createElement('div');
+    // div.setAttribute('id', 'opentok_publisher');
+    // parentDiv.appendChild( div );
+    // publisher.addEventListener('settingsButtonClick', callback );
   },
  settingsButtonClickHandler : function( event ) {
     event.preventDefault();
